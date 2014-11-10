c3c527d416cf76a9baaa5c027f9f6e46da90da0b
jonthedecepticon <jlambson@hirevue.com>
2014-11-10 10:50:14 -0700

    updated

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 'use strict';
 
 var express = require('express'),
-bodyParser = require('body-parser'),
-cors = require('cors'),
-port = 80,
-mongoose = require('mongoose'),
-passport = require('passport'),
-LocalStrategy = require('passport-local'),
-Schema = mongoose.Schema,
-session = require('express-session'),
-csv = require('csv'),
-request = require('request');
+	bodyParser = require('body-parser'),
+	cors = require('cors'),
+	port = 8888 || process.env.EXPRESS_PORT,
+	mongoose = require('mongoose'),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local'),
+	Schema = mongoose.Schema,
+	session = require('express-session'),
+	csv = require('csv'),
+	request = require('request');
 
 request = request.defaults({jar: true});
+	
+
 
 passport.use(new LocalStrategy({
-	usernameField: 'email',
-	passwordField: 'password'
+  usernameField: 'email',
+  passwordField: 'password'
 },
 function(email,  done) {
-	User.findOne({email: email}, function(err, user) {
-		if (err) {
-			return done(new Error("No user found with those credentials"));
-		}
-		return done(null, user);
-	});
+  User.findOne({email: email}, function(err, user) {
+    if (err) {
+      return done(new Error("No user found with those credentials"));
+    }
+    return done(null, user);
+  });
 }));
 
 passport.serializeUser(function(user, done) {
-	done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-	User.findById(id, function(err, user) {
-		done(err, user);
-	})
+  User.findById(id, function(err, user) {
+    done(err, user);
+  })
 });
 
 var User = mongoose.model('User', new Schema({
-	email: { type: String },
-	password: { type: String }
+  email: { type: String },
+  password: { type: String }
 }));
 
 //mongoose setup
@@ -57,18 +59,19 @@ app.use(passport.session());
 
 
 var authenticateUser = function(req, res, next) {
-	passport.authenticate('local', function(err, user, info) {
-		if (!user) {
-			return res.status(401).end();
-		}
-		req.logIn(user, function(err) {
-			return res.send('successfully signed in')
-		});
-	})(req, res, next);
+  passport.authenticate('local', function(err, user, info) {
+    if (!user) {
+      return res.status(401).end();
+    }
+    req.logIn(user, function(err) {
+      return res.send('successfully signed in')
+    });
+  })(req, res, next);
 }
 
 //login API endpoint
 app.post('/api/auth', authenticateUser);
+
 
 //LOGIN
 app.post('/hirevueLogin', function(req, res) {
@@ -76,19 +79,19 @@ app.post('/hirevueLogin', function(req, res) {
 	request.post({
 		url: 'https://app.devhv.com/api/v1/login/',
 		body: {
-			"applicationToken": "test_public_token",
-			"version": "1.2.0",
-			"impersonate": req.body.email,
-			"apiKey": process.env.HIREVUE_API 
-		},
+		    "applicationToken": "test_public_token",
+		    "version": "1.2.0",
+		    "impersonate": req.body.email,
+		    "apiKey": process.env.HIREVUE_API 
+			},
 		json: true
 	}, 
 	function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			return res.json(body);
-		}
-		console.log("error", response.statusCode);
-		return {error: error};
+	 	if (!error && response.statusCode == 200) {
+	    	return res.json(body);
+	 	}
+	 	console.log("error", response.statusCode);
+	 	return {error: error};
 	});
 });
 
@@ -105,11 +108,11 @@ app.get('/firevue', function(req, res) {
 		json: true
 	}, 
 	function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			return res.json(body);
-		}
-		console.log("error", response.statusCode);
-		return {error: error};
+	 	if (!error && response.statusCode == 200) {
+	    	return res.json(body);
+	 	}
+	 	console.log("error", response.statusCode);
+	 	return {error: error};
 	});
 });
 
@@ -122,22 +125,22 @@ app.post('/hirevueLogout', function(req, res) {
 	}, 
 	function (error, response, body) {
 		console.log(response.statusCode);
-		if (!error && response.statusCode == 200) {
-			return res.json(body);
-		}
-		console.log("error", response.statusCode);
-		return {error: error};
+	 	if (!error && response.statusCode == 200) {
+	    	return res.json(body);
+	 	}
+	 	console.log("error", response.statusCode);
+	 	return {error: error};
 	});
 });
 
 var requireAuth = function(req, res, next) {
-	if (!req.isAuthenticated()) {
-		return res.status(401).end();
-	}
-	next();
+  if (!req.isAuthenticated()) {
+    return res.status(401).end();
+  }
+  next();
 };
 app.get('/api/user/me', requireAuth, function(req, res) {
-	return res.json(req.user);
+  return res.json(req.user);
 });
 
 // mongoose.connect(databaseRefrence);
